57c34feb019145203b430a0cf51186c714b717ad
zafarivaev <z.ivaev@mail.ru>
2019-11-28 11:19:11 +0500

    Initial Commit

---
+++ Landmarks/Models/UserData.swift
@@ -0,0 +1,13 @@
+/*
+
+A model object that stores app data.
+*/
+
+import Combine
+import SwiftUI
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+    @Published var profile = Profile.default
+}
---
+++ Landmarks/Supporting Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+/*
+
+A view that displays a rotated version of a badge symbol.
+*/
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+        .padding(-60)
+        .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
